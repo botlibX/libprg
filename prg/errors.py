258559e2f9f460bca529a7149f8e0820eb770671
botlib <botlib@proton.me>
2023-11-03 21:34:16 +0100

    add errors

---
+++ prg/errors.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# This file is placed in the Public Domain.
+#
+#
+
+
+"errors"
+
+
+import io
+import traceback
+
+
+from .object import Object
+
+
+class Censor(Object):
+
+    output = None
+    words = []
+
+    @staticmethod
+    def skip(txt) -> bool:
+        for skp in Censor.words:
+            if skp in str(txt):
+                return True
+        return False
+
+
+class Errors(Object):
+
+    errors = []
+
+    @staticmethod
+    def add(exc) -> None:
+        excp = exc.with_traceback(exc.__traceback__)
+        Errors.errors.append(excp)
+
+    @staticmethod
+    def format(exc) -> str:
+        res = ""
+        stream = io.StringIO(
+                             traceback.print_exception(
+                                                       type(exc),
+                                                       exc,
+                                                       exc.__traceback__
+                                                      )
+                            )
+        for line in stream.readlines():
+            res += line + "\n"
+        return res
+
+    @staticmethod
+    def handle(exc) -> None:
+        if Censor.output:
+            Censor.output(Errors.format(exc))
+
+    @staticmethod
+    def show() -> None:
+        for exc in Errors.errors:
+            Errors.handle(exc)

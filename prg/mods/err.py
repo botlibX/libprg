fe04fafecc4a4e00c5990422c550aa3d31831d91
botlib <botlib@proton.me>
2023-11-05 00:30:09 +0100

    add err command

---
+++ prg/mods/err.py
@@ -0,0 +1,17 @@
+# This file is placed in the Public Domain.
+#
+# pylint: disable=E0402,C0116
+
+
+"show errors"
+
+
+from ..error import Errors
+
+
+def err(event):
+    if not Errors.errors:
+        event.reply("no errors")
+        return
+    for exc in Errors.errors:
+        event.reply(Errors.format(exc))
